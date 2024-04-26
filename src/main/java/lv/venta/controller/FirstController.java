0fbb63fe30cf27d99199f27e7bc9368a7e1888bc
andzekausk <s23kausandz@venta.lv>
2024-04-26 11:31:23 +0300

    Add: CRUD controller

--- src/main/java/lv/venta/controller/FirstController.java
+++ src/main/java/lv/venta/controller/FirstController.java
@@ -52,4 +52,5 @@ public class FirstController {
 		return "product-show-all";
 	}
 
+
 }
---
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -0,0 +1,52 @@
+package lv.venta.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lv.venta.model.Product;
+import lv.venta.service.IProductCRUDService;
+
+@Controller
+@RequestMapping("/product/crud")
+public class ProductCRUDController {
+	
+	@Autowired
+	private IProductCRUDService crudService;
+	
+	@GetMapping("/all") // localhost:8080/product/crud/all
+	public String getProductCRUDAll(Model model) {
+		
+		try {
+			model.addAttribute("mydata", crudService.retrieveAll());
+			return "product-show-all";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			model.addAttribute("msg", e.getMessage());
+			return "error-page";
+		}
+		
+	}
+	
+	@GetMapping("/{id}") // localhost:8080/product/crud/id
+	public String getProductCRUD(@PathVariable("id") int id, Model model) {
+		
+		try {
+			model.addAttribute("mydata", crudService.retrieveById(id));
+			return "product-show-one-page";
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			model.addAttribute("msg", e.getMessage());
+			return "error-page";
+		}
+		
+	}
+}
