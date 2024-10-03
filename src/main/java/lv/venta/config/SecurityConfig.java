1d448c40744db361a209f84e35a22311cb3a48d5
andzekausk <s23kausandz@venta.lv>
2024-10-03 14:28:00 +0300

    edit: changed SecurityConfig to work with db

--- src/main/java/lv/venta/config/SecurityConfig.java
+++ src/main/java/lv/venta/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package lv.venta.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -17,31 +18,47 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 	
 	@Bean
-	public UserDetailsManager createDemoUsers() {
-		
+	public MyUserDetailsManager getDetailsService() {
+		return new MyUserDetailsManager();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider createProvider() {
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
 		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 		
-		UserDetails details1 = User.builder()
-				.username("andzejs.kauskals")
-				.password(encoder.encode("123"))
-				.authorities("USER")
-				.build();
-
-		UserDetails details2 = User.builder()
-				.username("janis.berzins")
-				.password(encoder.encode("234"))
-				.authorities("ADMIN")
-				.build();
-		
-		UserDetails details3 = User.builder()
-				.username("liga.jauka")
-				.password(encoder.encode("345"))
-				.authorities("USER")
-				.build();
-		
-		return new InMemoryUserDetailsManager(details1, details2, details3);
+		provider.setPasswordEncoder(encoder);
+		provider.setUserDetailsService(getDetailsService());
+		return provider;
 	}
+	
+//	@Bean
+//	public UserDetailsManager createDemoUsers() {
+//		
+//		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//		
+//		UserDetails details1 = User.builder()
+//				.username("andzejs.kauskals")
+//				.password(encoder.encode("123"))
+//				.authorities("USER")
+//				.build();
+//
+//		UserDetails details2 = User.builder()
+//				.username("janis.berzins")
+//				.password(encoder.encode("234"))
+//				.authorities("ADMIN")
+//				.build();
+//		
+//		UserDetails details3 = User.builder()
+//				.username("liga.jauka")
+//				.password(encoder.encode("345"))
+//				.authorities("USER")
+//				.build();
+//		
+//		return new InMemoryUserDetailsManager(details1, details2, details3);
+//	}
 
+	@Bean
 	public SecurityFilterChain configurePermissionToEndpoints(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth->auth
 				.requestMatchers("/hello/**").permitAll()
